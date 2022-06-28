47485d13ec387e627c47daa4d290d256e7b50c1c
the-gigi <the.gigi@gmail.com>
2022-06-27 23:16:27 -0700

    with empty test

--- cmd/hello_test.go
+++ cmd/hello_test.go
@@ -3,10 +3,30 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sumi")
-	want := "Hello, Sumi"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	//  In Go you can declare functions inside other functions and assign them to variables.
+	// You can then call them, just like normal functions.
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in our function
+		// call rather than inside our test helper.
+		t.Helper()
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to actual people", func(t *testing.T) {
+		got := Hello("Sumi")
+		want := "Hello, Sumi"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("saying hello to empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
