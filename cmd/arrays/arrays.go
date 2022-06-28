a2fff5296b847202f535235d04d4eaf79083c9d1
the-gigi <the.gigi@gmail.com>
2022-06-28 12:50:54 -0700

    arrays and slice

--- cmd/arrays/arrays.go
+++ cmd/arrays/arrays.go
@@ -1,8 +1,19 @@
 package arrays
 
-func Sum(arr [5]int) (sum int) {
+func Sum(arr []int) (sum int) {
 	for _, val := range arr {
 		sum += val
 	}
 	return
+}
+
+func SumAll(arr ...[]int) []int {
+	lenarr := len(arr)
+	sums := make([]int, lenarr)
+
+	for i, num := range arr {
+		println(num)
+		sums[i] = Sum(num)
+	}
+	return sums
 }
--- cmd/arrays/arrays_test.go
+++ cmd/arrays/arrays_test.go
@@ -1,15 +1,48 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	assertCorrectMessage := func(t testing.TB, got, want int) {
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in our function
+		// call rather than inside our test helper.
+		t.Helper()
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
 	}
+
+	assertCorrectMessageArr := func(t testing.TB, gotarr, wantarr []int) {
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in our function
+		// call rather than inside our test helper.
+		t.Helper()
+		if !reflect.DeepEqual(gotarr, wantarr) {
+			t.Errorf("Got %q want %q", gotarr, wantarr)
+		}
+	}
+
+	t.Run("Sum multiple arrays", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		assertCorrectMessageArr(t, got, want)
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertCorrectMessage(t, got, want)
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
